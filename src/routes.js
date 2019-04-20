ad2346b429a25642c3f55a97d80b4c30b33c66ba
João Wicktor <joaowicktor@gmail.com>
2019-04-20 19:42:18 -0300

    Adding _redirect file to use with Netlify

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={ Main } />
       <Route path="/details" component={ CharacterDetails } />
-      <Route path='*' component={ NotFound } />
+      <Route path='/not-found' component={ NotFound } />
     </Switch>
   </BrowserRouter>
 );
